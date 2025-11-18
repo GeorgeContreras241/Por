7e4085cf4a8b1644ec2731a7e6907d427a291a8c
GeorgeContreras241 <maicolcontreras241@gmail.com>
2025-11-18 17:15:18 +0100

    update: projects

--- src/const/projects.js
+++ src/const/projects.js
@@ -41,7 +41,7 @@ export const projects = [
         { name: 'MongoDB', icon: 'Mongo' }
       ],
       url: false,
-      github: 'https://github.com/GeorgeContreras241/G-MetalicasJyN',
+      github: 'https://github.com/GeorgeContreras241/Ward-Spy',
       image: './images/project01.webp'
     },
     {
